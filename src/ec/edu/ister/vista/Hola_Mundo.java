1284efcae295a21b5fe7750d1f3c3028c4419eae
LuisChaqui <luigi92pm@gmail.com>
2018-10-29 10:55:09 -0500

    se implemento la clase punto

---
+++ src/ec/edu/ister/vista/Hola_Mundo.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.ister.vista;
+
+/**
+ *
+ * @author LAB5-PC8-USUARIO
+ */
+public class Hola_Mundo {
+   public static void main(String[] args) {  
+                   System.out.print("Hola Mundo!");
+       }
+ 
+    
+}
